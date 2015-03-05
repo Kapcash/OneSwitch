a8945e13083f6bd7b8fe24a90294c5ef70d1d2e5
Mehdi Haddad <haddad.e1301472@etud.univ-ubs.fr>
2015-03-05 15:40:21 +0100

    fix bugs Démmare au démmarage du téléphone

--- OneSwitch/src/com/example/oneswitch/app/OneSwitchActivity.java
+++ OneSwitch/src/com/example/oneswitch/app/OneSwitchActivity.java
@@ -1,12 +1,14 @@
 package com.example.oneswitch.app;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
-import java.io.IOException;
 
 import com.example.oneswitch.R;
 import com.example.oneswitch.action.ActionButton;
@@ -14,27 +16,43 @@ import com.example.oneswitch.action.ActionButton;
 public class OneSwitchActivity extends Activity{
 	private Intent globalService;
 	private boolean isStarted = false;
+	private Button startStopBt;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_one_switch);
+		init();
+		listener();
+		
+		
+	}
+	
+	private void init(){
+		startStopBt = (Button) findViewById(R.id.button1);
+		if(isMyServiceRunning(OneSwitchService.class)){
+			isStarted=true;
+			startStopBt.setText("Stop Service");
+		}
 		globalService = new Intent(this, OneSwitchService.class);
 	}
-
-	public void buttonClicked(View v){
-		if(!isStarted){
-			startService(globalService);
-			isStarted = true;
-			((Button)v).setText("Stop Service");
-
-		}
-		else{
-			stopService(globalService);
-			isStarted = false;
-			((Button)v).setText("Start Service");
-		}
-
+	
+	private void listener(){
+		startStopBt.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				if(!isStarted){
+					startService(globalService);
+					isStarted = true;
+					((Button)v).setText("Stop Service");
+				}
+				else{
+					stopService(globalService);
+					isStarted = false;
+					((Button)v).setText("Start Service");
+				}
+			}
+		});
 	}
 
 	@Override
@@ -44,7 +62,16 @@ public class OneSwitchActivity extends Activity{
 		}
 		else{
 			super.onBackPressed();
-		}
-			
+		}	
+	}
+	
+	private boolean isMyServiceRunning(Class<?> serviceClass) {
+	    ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+	    for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+	        if (serviceClass.getName().equals(service.service.getClassName())) {
+	            return true;
+	        }
+	    }
+	    return false;
 	}
 }
